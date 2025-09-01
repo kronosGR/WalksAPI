8edb9bc9ea4c97047ee6761977e7a725c910b5be
kronosGR <geo.elgeo@gmail.com>
2025-09-01 13:01:06 +0200

    added dbcontext to service and first migration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<WalksDbContext>(options => options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnectionString"),
+    new MySqlServerVersion(new Version(8, 0, 33))
+    ));
 
 var app = builder.Build();
 
